a1f2d75420ddaaeed2ca023f3b4142244e8c466d
Ulyana <ulianabober@gmail.com>
2024-07-11 19:39:58 +0300

    Add ad timer

--- CubeGame/Assets/Source/Scripts/UI/AdsWindow.cs
+++ CubeGame/Assets/Source/Scripts/UI/AdsWindow.cs
@@ -1,14 +1,45 @@
 using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using YG;
 
 public class AdsWindow : MonoBehaviour
 {
     public Action OnContinue;
+    [SerializeField] private int _lifeTime;
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private GameButtons _gameButton;
+    private Coroutine _lifeTick;
     
     private void OnEnable()
     {
         YandexGame.RewardVideoEvent += Rewarded;
+        StartTimer();
+    }
+    
+    private void OnDisable()
+    {
+        YandexGame.RewardVideoEvent -= Rewarded;
+        StopTimer();
+    }
+
+    private void StartTimer()
+    {
+        _lifeTick = StartCoroutine(LifeTick());
+    }
+    
+    public void OpenAds()
+    {
+        YandexGame.RewVideoShow(1);
+        StopTimer();
+    }
+
+    private void StopTimer()
+    {
+        if (_lifeTick != null)
+            StopCoroutine(_lifeTick);
     }
 
     private void Rewarded(int i)
@@ -16,14 +47,14 @@ public class AdsWindow : MonoBehaviour
         gameObject.SetActive(false); 
         OnContinue?.Invoke();
     }
-    
-    private void OnDisable()
-    {
-        YandexGame.RewardVideoEvent -= Rewarded;
-    }
 
-    public void OpenAds()
+    private IEnumerator LifeTick()
     {
-        YandexGame.RewVideoShow(1);
+        for (int i = 0; i < _lifeTime; i++)
+        {
+            _text.text = (_lifeTime - i).ToString();
+            yield return new WaitForSeconds(1);
+        }
+        _gameButton.Exit();
     }
 }
